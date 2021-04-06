45dc19e2efd99d69c36c5b00ff04df40c9055f10
buibaouet <buivbao.uet@gmail.com>
2021-04-06 21:43:46 +0700

    màn mượn sách admin

---
+++ library-manage/Library.Management/Library.Management.Web/wwwroot/js/admin/bookborrow.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    var bookborrow = new BookBorrow();
+});
+
+class BookBorrow {
+    constructor() {
+        this.initEvents();
+    }
+
+    initEvents() {
+        $('.img-circle').hover(function (e) {
+            $('div#pop-up').show();
+            //.css('top', e.pageY + moveDown)
+            //.css('left', e.pageX + moveLeft)
+            //.appendTo('body');
+        }, function () {
+            $('div#pop-up').hide();
+        });
+
+        $('.img-circle').mousemove(function (e) {
+            $("div#pop-up").css('top', e.pageY - 80).css('left', e.pageX - 180);
+        });
+    }
+}
